dc3849e237cc6f25e7d08ba66957f4ca27be07c9
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-05-28 00:20:39 +0400

    skeleton

--- src/public/index.php
+++ src/public/index.php
@@ -2,6 +2,7 @@
 <head>
     <title>Users profile</title>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
 <ul>
@@ -12,8 +13,6 @@
 </html>
 
 <?php
-
-use App\ApiControllers\FetchApi;
 
 require __DIR__ . '/../vendor/autoload.php';
 
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["src/public/index.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-children'),
+  ],
+}
