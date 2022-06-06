c5e1dcd91a00c1371751e7cecefb260dc6f4b8d2
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-06 16:11:26 +0400

    change source path

--- src/views/sources/index.php
+++ src/views/sources/index.php
@@ -4,4 +4,4 @@
         color: blue;
     }
 </style>
-<h4 class="link"> <a target="_blank" href="https://github.com/George1Meshveliani/github-users-info" >The link can only be opened by a github user who has an access to the repository</a> </h4>
+<h4 class="link"> <a target="_blank" href="https://github.com/George1Meshveliani/api-task-test-bootcamp-3" >The link can only be opened by a github user who has an access to the repository</a> </h4>
