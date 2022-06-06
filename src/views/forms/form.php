a632d9c5002a3f34313fc8e93e6285fcde0aec58
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-06 21:53:02 +0400

    delete extra directory:

--- src/views/forms/form.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="/">
-    <label>Enter a github user name</label>
-    <input type="text" name="userName"/>
-    <input type="submit" value="Find information about the user" name="submit">
-</form>
