665d117ae0b58ceec64a83b00d4834186b620d02
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-06 16:05:28 +0400

    change a title of the project

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Users profile</title>
+    <title>Girls Shows</title>
     <link rel="stylesheet" href="styles.css">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
