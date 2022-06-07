fb049d0995ea35abdae3a65919e6350c0792df44
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-07 23:59:53 +0400

    delete extra lines

--- src/app/PageControllers/DashboardController.php
+++ src/app/PageControllers/DashboardController.php
@@ -23,14 +23,6 @@ class DashboardController
         $stmt = $db->query("SELECT * FROM shows_list");
         $res =  $stmt->fetchAll();
 
-        var_dump($res);
-//        $row_array_zero = [];
-//        $row_array_one = [];
-//        while ($rows = $stmt->fetch(PDO::FETCH_NUM)) {
-//            $row_array_zero = $rows[0];
-//            $row_array_one  = $rows[1];
-//        }
-//        var_dump($row_array_zero);
 
         $showsForm = View::make('shows/SearchBar', [
                 'value0' => $value0,
